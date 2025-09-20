b3f72b34ba7fd61a24be0c577712b434919510c7
YetiOffice <office@yetiwelding.com>
2025-09-20 10:43:49 -0600

    Update priceMap.js

--- data/priceMap.js
+++ data/priceMap.js
@@ -3,7 +3,7 @@
 // Replace 'price_xxx...' with your real Stripe Price IDs.
 
 export const PRICE_MAP = {
-  "patio-pro-12x12": "price_xxxxxxxxxxxxxxxxxx",
-  "patio-pro-12x16": "price_yyyyyyyyyyyyyyyyyy",
-  "patio-pro-12x20": "price_zzzzzzzzzzzzzzzzzz",
+  "patio-pro-12x12": "price_1S9TqbJvCA6mRFZEbHL1fhxH",
+  "patio-pro-12x16": "price_1S9TqwJvCA6mRFZEoe90y6By",
+  "patio-pro-12x20": "price_1S9TrHJvCA6mRFZEIftBXEfB",
 };
